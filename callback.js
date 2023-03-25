b617e645816071b966c139a78d3a9c9cfe28d99d
eda inal <edainalisil@gmail.com>
2023-03-25 14:19:53 +0300

    first commit

---
+++ callback.js
@@ -0,0 +1,17 @@
+// setTimeout(()=>{
+//     console.log("hello")
+// },4000);
+// const sayHi= (cb) => {
+// cb();
+// }
+// sayHi(()=>{
+//     console.log("hello")
+    
+// })
+
+(async()=>{
+  const hello = await
+  await console.log("hello");
+  const again = await
+  await console.log("again");
+})()
---
+++ myModule.js
@@ -0,0 +1,6 @@
+const hello = (message) => console.log("hello" + message);
+ const topla = (a,b) =>a+b;
+hello(" Eda");
+const cikar = (a,b)=> a-b;
+
+export {hello,topla, cikar};
